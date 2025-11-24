505de6c40641e237bd8dc236ee1a1b3447cae33b
sowmya-cpu <gvvsowmya16@gmail.com>
2025-11-24 11:37:44 +0530

    Make API base URL configurable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api"
 });
+
 
 // attach token
 API.interceptors.request.use((config) => {
